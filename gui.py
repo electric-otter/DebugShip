2ada460df7981d8b043dbd619a706d18d3357c7c
electric-otter <krw122081@gmail.com>
2024-12-24 06:01:55 +0000

    Update gui.py

--- gui.py
+++ gui.py
@@ -5,6 +5,5 @@ ui.label("DebugShip")
 
 # Create a button with an action
 ui.button('Debug', on_click=lambda: [ui.notify('Debugging...'), open("ShipFramework.sh")])
-
 # Start the NiceGUI application
 ui.run()
