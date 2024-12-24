6a0605528e0ea35f038c7e5b9ae73059817dd51b
electric-otter <krw122081@gmail.com>
2024-12-24 06:27:04 +0000

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,9 +1,9 @@
 from nicegui import ui
-
+import os
 # Create a label
 ui.label("DebugShip")
 
 # Create a button with an action
-ui.button('Debug', on_click=lambda: [ui.notify('Debugging...'), open("ShipFramework.sh")])
+ui.button('Debug', on_click=lambda: [ui.notify('Debugging...'), open ("openkitty.sh")])
 # Start the NiceGUI application
 ui.run()
