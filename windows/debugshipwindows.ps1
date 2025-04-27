4b1d0036736ebbb0db075fe7397d9febed7f0a3d
electric-otter made Unxus! <electric-otter-github@proton.me>
2025-04-27 16:58:45 -0400

    Update debugshipwindows.ps1

--- windows/debugshipwindows.ps1
+++ windows/debugshipwindows.ps1
@@ -1,7 +1,8 @@
 # Setting up variables
 $x = 10
 $y = 20
-
+$GPU = (Get-WmiObject Win32_VideoController).Name
+$cpuInfo = Get-CimInstance -ClassName Win32_Processor
 # Set breakpoints on variables to pause execution when they are accessed or changed
 Set-PSBreakpoint -Variable x
 Set-PSBreakpoint -Variable y
@@ -17,7 +18,7 @@ Wait-Debugger
 # Make the options for target devices or 
 $options = "$targetDevice $ForEach-Object"
 # Construct the command string
-$debugger = "debugger --target $targetDevice $options $PSBoundParameters $Args Set-PSBreakPoint $procInfo"
+$debugger = "debugger --target $targetDevice $options $PSBoundParameters $Args Set-PSBreakPoint $procInfo $GPU $cpuInfo"
 
 
 Invoke-Expression $debugger
